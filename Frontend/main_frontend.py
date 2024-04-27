2fe6f4b3d6f504073efc0d0ff17a6c8b1b2b4f6b
sid12153 <77562156+sid12153@users.noreply.github.com>
2024-04-27 00:06:17 -0400

    Update main_frontend.py

--- Frontend/main_frontend.py
+++ Frontend/main_frontend.py
@@ -3,7 +3,7 @@ import requests
 import datetime
 import json
 from streamlit_lottie import st_lottie
-from streamlit_extras.switch_page_button import switch_page
+# from streamlit_extras.switch_page_button import switch_page
 from PIL import Image
 import time
 
